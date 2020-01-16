ecfded105fa38e4c4a9d2b234e5124861f055945
Mateusz <mateuszlatkowski@smcebi.edu.pl>
2020-01-16 21:27:35 +0100

    Fix small warnings.

--- app/src/main/java/com/example/newsapp/Category.java
+++ app/src/main/java/com/example/newsapp/Category.java
@@ -4,15 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.icu.text.ScientificNumberFormatter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import javax.xml.parsers.FactoryConfigurationError;
 
 public class Category extends AppCompatActivity{
 
