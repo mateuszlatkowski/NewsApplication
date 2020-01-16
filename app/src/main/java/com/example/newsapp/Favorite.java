df6f7d3bf4100b43d0128762ce59eab1e0d5dd84
Mateusz <mateuszlatkowski@smcebi.edu.pl>
2020-01-16 21:30:24 +0100

    Fix small warnings and change method articles are displayed from the database.

--- app/src/main/java/com/example/newsapp/Favorite.java
+++ app/src/main/java/com/example/newsapp/Favorite.java
@@ -3,20 +3,28 @@ package com.example.newsapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.Cursor;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class Favorite extends AppCompatActivity {
 
-    DatabaseHelper myDB;
+    private ListView listView;
+    private FirebaseListAdapter adapter;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +32,61 @@ public class Favorite extends AppCompatActivity {
         setContentView(R.layout.activity_favorite);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ListView listView = findViewById(R.id.listFavorite);
-        myDB = new DatabaseHelper(this);
+        listView = findViewById(R.id.listFavorite);
+        Query query = FirebaseDatabase.getInstance().getReference().child("Favorites");
+        FirebaseListOptions<DatabaseHelper> options = new FirebaseListOptions.Builder<DatabaseHelper>()
+                .setLayout(R.layout.list_favorite)
+                .setLifecycleOwner(Favorite.this)
+                .setQuery(query, DatabaseHelper.class)
+                .build();
 
-        ArrayList<String> theList = new ArrayList<>();
-        Cursor data = myDB.getListContents();
+        adapter = new FirebaseListAdapter(options) {
+            @Override
+            protected void populateView(View v, Object model, int position) {
+                TextView title = v.findViewById(R.id.textViewTitleFavorite);
+                TextView description = v.findViewById(R.id.textViewTitleDescriptionFavorite);
+                TextView author = v.findViewById(R.id.textViewAuthorFavorite);
+                TextView date = v.findViewById(R.id.textViewTimeFavorite);
+                ImageView image = v.findViewById(R.id.imageViewArticleFavorite);
 
-        if(data.getCount() == 0){
-            Toast.makeText(this, "Lista ulubionych artykułów jest pusta!", Toast.LENGTH_LONG).show();
-        } else {
-            while(data.moveToNext()){
-                theList.add(data.getString(1));
-                ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, theList);
-                listView.setAdapter(listAdapter);
+                DatabaseHelper databaseHelper = (DatabaseHelper) model;
+                title.setText(databaseHelper.getTitle().toString());
+                description.setText(databaseHelper.getDescription().toString());
+                author.setText(databaseHelper.getAuthor().toString());
+                date.setText(databaseHelper.getDate().toString());
+
+                RequestOptions requestOptions = new RequestOptions()
+                        .centerCrop()
+                        .placeholder(R.mipmap.ic_launcher_round)
+                        .error(R.mipmap.ic_launcher_round);
+
+                Glide.with(Favorite.this).load(databaseHelper.getURL_Image().toString()).apply(requestOptions).into(image);
+
+                url = databaseHelper.getURL().toString();
             }
-        }
+        };
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(Favorite.this, ArticleActivity.class);
+                intent.putExtra("url", url);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     @Override
@@ -54,4 +102,5 @@ public class Favorite extends AppCompatActivity {
     public boolean OnCreateOptionsMenu(Menu menu) {
         return true;
     }
+
 }
